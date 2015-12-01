636c050acbf0902cb7255e58118aa2e4773e15ba
Skyost <skyost@mail.com>
2015-12-01 23:08:56 +0100

    Préparation pour la reconnaissance des traits du visage :-)

--- src/fr/hdelaunay/image/utils/OpenCVUtils.java
+++ src/fr/hdelaunay/image/utils/OpenCVUtils.java
@@ -13,21 +13,38 @@ import org.opencv.objdetect.CascadeClassifier;
 import fr.hdelaunay.image.Main;
 
 public class OpenCVUtils {
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
 
 	/**
+	 * Cherche les visages sur l'image sélectionnée.
+	 * <br>https://github.com/Itseez/opencv/tree/master/data (les différents xml possibles).
 	 * 
-	 * <br>https://github.com/Itseez/opencv/tree/master/data.
+	 * @param input L'image.
 	 * 
-	 * @param input
-	 * @return
+	 * @return Le contour de ces visages.
 	 */
 
 	public static final Rect[] getFaces(final BufferedImage input) {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		return getTraits(input, "lbpcascade_frontalface.xml");
+	}
+	
+	/**
+	 * Cherche les traits du visage spécifié par le fichier xml.
+	 * 
+	 * @param input Le visage.
+	 * @param xml Les traits.
+	 * 
+	 * @return Les traits de ce visage.
+	 */
+	
+	public static final Rect[] getTraits(final BufferedImage input, final String xml) {
 		final Mat image = new Mat(input.getWidth(), input.getHeight(), CvType.CV_8UC3);
 		image.put(0, 0, ((DataBufferByte)input.getRaster().getDataBuffer()).getData());
 		final MatOfRect faces = new MatOfRect();
-		new CascadeClassifier(Main.class.getResource(Main.RES_PACKAGE + "lbpcascade_frontalface.xml").getPath().substring(1)).detectMultiScale(image, faces);
+		new CascadeClassifier(Main.class.getResource(Main.RES_PACKAGE + "opencv/" + xml).getPath().substring(1)).detectMultiScale(image, faces);
 		return faces.toArray();
 	}
 
